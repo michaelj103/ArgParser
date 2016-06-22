826a3ee1301895f17f28745bd409faadfcd8ccbc
Michael Brandt <michael_brandt@apple.com>
2016-06-22 01:01:56 -0700

    add base for Objective-C version

---
+++ ObjC/Source/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ArgumentParser
+//
+//  Created by Michael Brandt on 6/22/16.
+//  Copyright © 2016 MJB. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
